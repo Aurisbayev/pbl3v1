5aab0b32a0857cbe2b1c20509cc9ce1e62093c3d
Aurisbayev <askar.urisbayev.mail@gmail.com>
2026-07-15 06:54:56 +0900

    Create __init__.py

---
+++ backend/services/__init__.py
@@ -0,0 +1,10 @@
+from .map_processing import (
+    MapProcessingError,
+    build_map_payload,
+)
+
+from .map_service import *
+from .navigation_processing import *
+from .navigation_service import *
+from .route_generator import *
+from .scenic_scorer import *
